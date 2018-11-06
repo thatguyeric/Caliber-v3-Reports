3ba6f468363193b3ce19ec10e0051020f2f80486
cburchet <cburchet@gmu.edu>
2018-11-06 15:07:24 -0500

    adding filter functionally for batches in a year

--- src/aura/reportsPageHeader/reportsPageHeaderHelper.js
+++ src/aura/reportsPageHeader/reportsPageHeaderHelper.js
@@ -1,12 +1,10 @@
 ({
 	buildBatchStrings : function(component) {
 		var action3 = component.get("c.buildBatchStrings");
-        var trainingsParam = component.get("v.allBatches");
-        console.log(trainingsParam);
+        var trainingsParam = component.get("v.allBatches");       
         action3.setParams({"trainings" : trainingsParam});
         action3.setCallback(this, function(response){
             var state = response.getState();
-            console.log(state);
             if (state === "SUCCESS"){
                 component.set("v.allBatchLabels", response.getReturnValue());
                 component.set("v.batchLabel", response.getReturnValue()[0]);
