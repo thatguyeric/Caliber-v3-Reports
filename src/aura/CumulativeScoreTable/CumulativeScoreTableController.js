971d8e7881024e976dbd117524180fa3a746c18a
Sandra <sandradeann94@gmail.com>
2018-11-07 14:35:50 -0500

    added revStyling and table component

---
+++ src/aura/CumulativeScoreTable/CumulativeScoreTableController.js
@@ -0,0 +1,27 @@
+({
+    onInit : function(component, event, helper) {
+        var action = component.get("c.getCalGrades");
+ 
+        // Create a callback that is executed after
+        // the server-side action returns
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set('v.calGrade', response.getReturnValue());
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+ 
+        $A.enqueueAction(action);
+    }
+})
